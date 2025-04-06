fa685c18351a6db937c64c7b0ea6e210c52aa32d
Rajkumar Avachar <rajavachar59@gmail.com>
2025-04-06 23:59:47 +0530

    update error.jsx

--- frontend/src/pages/error/Error.jsx
+++ frontend/src/pages/error/Error.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div>
-      <h2 className="text-center mt-5 p-5 fw-bold">
+    <div className="text-center border">
+      {/* <h1 className="text-center mt-5 p-5 fw-bold">
         <span className="text-danger fs-1">404</span>
         <br /> Page Not Found!
-      </h2>
+      </h1> */}
+      <img src="public/404.jpg" alt="404 Page Not Found" />
     </div>
   );
 };
