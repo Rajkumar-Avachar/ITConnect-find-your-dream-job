8ddb14e4d351312fa2d4fd84f9c4d6c234fb0427
Rajkumar Avachar <rajavachar59@gmail.com>
2025-10-03 00:51:39 +0530

    deployed

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,8 +1,12 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+    console.log(req.cookies);
+    console.log(token);
     if (!token) {
       return res.status(401).json({
         message: "Please login to continue",
