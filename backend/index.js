6013c24aabb20f2606aba608c78b02aac146ae65
Rajkumar Avachar <rajavachar59@gmail.com>
2025-10-02 19:42:22 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,13 @@ app.use(
         ? "https://itconnect.vercel.app"
         : "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+// handle preflight requests
+app.options("*", cors());
 
 // const allowedOrigins =
 //   process.env.NODE_ENV === "production"
@@ -43,7 +47,6 @@ app.use(
 //     credentials: true,
 //   })
 // );
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
