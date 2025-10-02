66432275f4abe5c89df3b26762a6105ef61bc575
Rajkumar Avachar <rajavachar59@gmail.com>
2025-10-02 18:35:29 +0530

    deployed

--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
-
+export const BASE_URL = "https://itconnectbackend.onrender.com/api/v1";
 export const USER_API = `${BASE_URL}/user`;
 export const JOBS_API = `${BASE_URL}/jobs`;
 export const COMPANY_API = `${BASE_URL}/companies`;
