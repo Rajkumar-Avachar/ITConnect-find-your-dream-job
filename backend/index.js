7a347b1ccaf454d4bf48189cd938629243b1bc09
Rajkumar Avachar <rajavachar59@gmail.com>
2025-10-13 13:33:51 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://jobgrids.onrender.com/"
+        ? "https://jobgrids.onrender.com"
         : "http://localhost:5173",
     credentials: true,
   })
